ff68e62223fd0866e11ae51ada3a42cb57c63a25
Reiz <52864970+sadeceoyun@users.noreply.github.com>
2021-11-13 18:37:45 +0300

    Add files via upload

---
+++ kullandığım programlar.cpp
@@ -0,0 +1,65 @@
+METİN2'de KULLANDIĞIM PROGRAMLAR.
+
+metin2 packalrı açmak için packmakerlite
+önerim (martysama)
+https://www.mediafire.com/folder/lwhcn5xyb53yq/PML
+daha hızlı ve donmadan açabiliyor.
+alternatif.
+epack (donabiliyor.)
+EterNexus
+
+Harita Düzenlemek İçin(martysama)
+https://www.mediafire.com/folder/g0r2rlnfewwv4/WE
+
+gr2leri 2.4 den 2.11 yükseltmek için GrannyConverter - by Helia01
+https://mega.nz/file/2d9gDALR#k5YWbBuYHaq1EgWEpJFpYT0Lp2il3l06iPn2CbkB2JQ
+
+.cppleri editlemek için
+https://notepad-plus-plus.org/
+
+vdi veya sunucuya bağlantı için kullanımı kolay
+https://filezilla-project.org/
+
+bu biraz daha özellikli
+https://winscp.net/eng/download.php
+örneğin link ekleyebiliyorsun.game sysmlink editleyebiliyorsun.
+
+mysql erişmek için.
+ücretsiz heidisql
+https://www.heidisql.com/
+
+ücretli navicat (son sürümde hatalar olursa 11. versiyon denenebilir)
+https://www.navicat.com/en/download/navicat-for-mysql
+
+dosyaları karşılaştırmak için.
+https://winmerge.org/
+örneğin temiz filesla editli filesi karşılaştırmak
+aradığınız sistemin bi filesden sökmek için.
+
+Dosyalarda kelime aramak için
+https://www.mythicsoft.com/agentransack/
+alternatif kelime aramak ve değiştirmek
+https://www.gezginler.net/indir/grepwin.html
+kelime arama ve değiştirme notepad++ da yapıyorum
+örneğin.
+ctrl f dosyalarda bul tümünü değiştir.
+
+ve pcde aradığınız dosyayı bulmak için.windows aramanın gelişmişi olarak düşünebilir.
+https://www.voidtools.com/tr-tr/
+
+client build etmek için
+https://visualstudio.microsoft.com/tr/vs/
+Community yeterli.kurulumdada c++ yerlerini seçmeniz yeterli.
+
+sanal pcden metin2 kurulum için.
+oracle virtualbox
+
+freebsd için.bence en hızlısı freebsd 13.0
+mysql tercihi mariadb103
+sql tabloalrı tecihi Aria (aria innodb yi googleden araştırarak alternatiflerinide inceleyebilirsiniz.)
+
+
+
+
+
+
